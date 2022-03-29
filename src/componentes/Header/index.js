95fd5e11acaf17558b05e5c5d6b7a5d51d327d4d
voncardoso <vonharrison26@gmail.com>
2022-03-29 15:59:09 -0300

    h4 plano de trabalho finalizado

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -4,10 +4,13 @@ import './style.css'
 
 export const Header = () =>{
     return(
+        <>
         <header>
             <img src={Brasao} alt="brasao do municipio" />
             <h4>PREFEITURA MUNICIPAL DE ANANNINDEU <br/> SECRETARIA MUNICIAPAL DE SANEAMENTO E INFRA ESTRUTURA</h4>
             <img src={Bandeira} alt="brasao do municipio" />
         </header>
+        <h4 className='h4planoTrabalho'>PLANO DE TRABALHO</h4>
+        </>
     )
 }
