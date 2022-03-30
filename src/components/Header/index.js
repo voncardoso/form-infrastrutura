5e252ed5d7dda483992f3f75acb1807ad8ab5a0f
voncardoso <vonharrison26@gmail.com>
2022-03-30 11:10:19 -0300

    reponsividade das img da header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,16 @@
 import Brasao from '../../assets/Brasao.svg';
 import Bandeira from '../../assets/Bandeira.svg';
-import './style.css'
+import {Container} from './style.js'
 
 export const Header = () =>{
     return(
-        <>
-        <header>
-            <img src={Brasao} alt="brasao do municipio" />
-            <h4>PREFEITURA MUNICIPAL DE ANANNINDEU <br/> SECRETARIA MUNICIAPAL DE SANEAMENTO E INFRA ESTRUTURA</h4>
-            <img src={Bandeira} alt="brasao do municipio" />
-        </header>
-        <h4 className='h4planoTrabalho'>PLANO DE TRABALHO</h4>
-        </>
+        <Container>
+            <header>
+                <img src={Brasao} alt="brasao do municipio" />
+                <h4>PREFEITURA MUNICIPAL DE ANANNINDEU <br/> SECRETARIA MUNICIAPAL DE SANEAMENTO E INFRA ESTRUTURA</h4>
+                <img src={Bandeira} alt="brasao do municipio" />
+            </header>
+            <h4 className='h4planoTrabalho'>PLANO DE TRABALHO</h4>
+        </Container>
     )
 }
---
+++ src/components/Header/style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+
+export const Container = styled.section`
+    header{
+        max-width: 1280px;
+        display: flex;
+        justify-content: space-between;
+        margin: 30px auto 0px auto;
+        height: 100px;
+    }
+
+    header h4{
+        font-size: 1.375rem;
+        font-weight: bold;
+        text-align: center;
+        display: flex;
+        align-self: center;
+        line-height: 30px;
+    }
+
+    img{
+        max-width: 193px;
+        width: 100%;
+    }
+
+    .h4planoTrabalho{
+        margin-top: 60px;
+        text-align: center;
+        font-size: 1.25rem;
+    }
+`
+
