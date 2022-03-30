f06c79fd6b501267d9b4e34cd2b5980835ad39bb
voncardoso <vonharrison26@gmail.com>
2022-03-30 11:21:02 -0300

    responsividade header

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -8,26 +8,66 @@ export const Container = styled.section`
         justify-content: space-between;
         margin: 30px auto 0px auto;
         height: 100px;
+        h4{
+            font-size: 1.375rem;
+            font-weight: bold;
+            text-align: center;
+            display: flex;
+            align-self: center;
+            line-height: 30px;
+        }
+
+        img{
+            max-width: 193px;
+            width: 100%;
+        }
     }
 
-    header h4{
-        font-size: 1.375rem;
-        font-weight: bold;
-        text-align: center;
-        display: flex;
-        align-self: center;
-        line-height: 30px;
-    }
-
-    img{
-        max-width: 193px;
-        width: 100%;
-    }
+   
 
     .h4planoTrabalho{
         margin-top: 60px;
         text-align: center;
         font-size: 1.25rem;
+    }
+
+    @media (max-width: 900px) {
+        header h4{
+            font-size: 1.125rem;
+        }
+
+        img{
+            max-width: 150px;
+            width: 100%;
+        }
+        .h4planoTrabalho{
+            font-size: 1.125rem;
+        }
+    }
+
+    @media (max-width: 690px) {
+        h4{
+            text-align: center;
+            margin: 0 auto;
+        }
+        img{
+            display: none;
+        }
+    }
+
+    @media (max-width: 450px) {
+       header{
+            h4{
+                font-size: 1rem;
+            }
+       }
+        img{
+            display: none;
+        }
+
+        .h4planoTrabalho{
+            font-size: 1rem;
+        }
     }
 `
 
